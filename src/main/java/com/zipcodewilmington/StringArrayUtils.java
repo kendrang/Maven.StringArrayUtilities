4713ac66625f1fa75b8e0fccc17e15b2feef4cf3
kendrang <kendrang@users.noreply.github.com>
2019-10-22 14:12:31 -0400

    6 more left

--- src/main/java/com/zipcodewilmington/StringArrayUtils.java
+++ src/main/java/com/zipcodewilmington/StringArrayUtils.java
@@ -25,7 +25,7 @@ public class StringArrayUtils {
      * @return last element in specified array
      */ // TODO
     public static String getLastElement(String[] array) {
-        return null;
+        return array[array.length-1];
     }
 
     /**
@@ -33,7 +33,7 @@ public class StringArrayUtils {
      * @return second to last element in specified array
      */ // TODO
     public static String getSecondToLastElement(String[] array) {
-        return null;
+         return array[array.length-2];
     }
 
     /**
@@ -42,7 +42,13 @@ public class StringArrayUtils {
      * @return true if the array contains the specified `value`
      */ // TODO
     public static boolean contains(String[] array, String value) {
-        return false;
+        boolean result = false;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == value) {
+                result = true;
+            }
+        }
+        return result;
     }
 
     /**
@@ -50,7 +56,11 @@ public class StringArrayUtils {
      * @return an array with identical contents in reverse order
      */ // TODO
     public static String[] reverse(String[] array) {
-        return null;
+        String[] result = new String[array.length];
+        for (int i =array.length-1; i>=0;i--){
+            result[array.length-1-i] = array[i];
+        }
+        return result;
     }
 
     /**
